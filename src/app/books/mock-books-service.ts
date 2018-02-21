93f9775be1b097ce6d39d1d2e8a04edc20cc3a9f
Blake Crowley <crowley.124@osu.edu>
2018-02-21 18:43:54 -0500

    Added angular code

---
+++ src/app/books/mock-books-service.ts
@@ -0,0 +1,6 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [
+    { id: 1, title: 'Ready Player One', author: 'Ernest Cline'},
+    { id: 2, title: 'Cathch 22', author: 'Jospeh Heller' }
+];
